ea9accd54ce21048752bf57993786df093e1825e
hamidraza1 <hrazaa306@gmail.com>
2020-06-09 17:36:20 +0200

    Updates

---
+++ precache-manifest.f339f3a32092c1c6a400b6371735853a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c23872bdaf48aa383e0bdc855d9769",
+    "url": "/BurgerBuilder/index.html"
+  },
+  {
+    "revision": "fb03332cb721f6a456df",
+    "url": "/BurgerBuilder/static/css/main.8a8a4153.chunk.css"
+  },
+  {
+    "revision": "c86e37f338952ac49498",
+    "url": "/BurgerBuilder/static/js/2.a4e873cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BurgerBuilder/static/js/2.a4e873cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb03332cb721f6a456df",
+    "url": "/BurgerBuilder/static/js/main.235b4c24.chunk.js"
+  },
+  {
+    "revision": "a28f8f73aee2ad9023b9",
+    "url": "/BurgerBuilder/static/js/runtime-main.20668b5e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder/static/media/28.1 burger-logo.b8503d26.png"
+  }
+]);
